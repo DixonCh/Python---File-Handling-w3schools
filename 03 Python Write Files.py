c00c648ed59c40401984e59933a008f4b82cb350
DixonCh <dixonchaudhary7@gmail.com>
2020-05-01 15:33:45 +0545

    Update 03 Python Write Files.py

--- 03 Python Write Files.py
+++ 03 Python Write Files.py
@@ -1,13 +1,17 @@
-Python File Write
+'''Python File Write:
+  
 Write to an Existing File
 To write to an existing file, you must add a parameter to the open() function:
+'''
 
 "a" - Append - will append to the end of the file
 
 "w" - Write - will overwrite any existing content
 
+'''
 Example
 Open the file "demofile2.txt" and append content to the file:
+'''
 
 f = open("demofile2.txt", "a")
 f.write("Now the file has more content!")
@@ -16,8 +20,11 @@ f.close()
 #open and read the file after the appending:
 f = open("demofile2.txt", "r")
 print(f.read())
+
+'''
 Example
 Open the file "demofile3.txt" and overwrite the content:
+'''
 
 f = open("demofile3.txt", "w")
 f.write("Woops! I have deleted the content!")
@@ -26,10 +33,13 @@ f.close()
 #open and read the file after the appending:
 f = open("demofile3.txt", "r")
 print(f.read())
+
+'''
 Note: the "w" method will overwrite the entire file.
 
 Create a New File
 To create a new file in Python, use the open() method, with one of the following parameters:
+'''
 
 "x" - Create - will create a file, returns an error if the file exist
 
@@ -37,13 +47,16 @@ To create a new file in Python, use the open() method, with one of the following
 
 "w" - Write - will create a file if the specified file does not exist
 
-Example
+#Example:
+'''
 Create a file called "myfile.txt":
+'''
 
 f = open("myfile.txt", "x")
+'''
 Result: a new empty file is created!
 
-Example
+Example:
 Create a new file if it does not exist:
-
+'''
 f = open("myfile.txt", "w")
